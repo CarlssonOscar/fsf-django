adb053bc53a84a8434879999db5551f7e1c167cb
CarlssonOscar <oosccar@gmail.com>
2021-11-25 11:53:57 +0000

    Add item names through __str__ function in models.py

--- todo/models.py
+++ todo/models.py
@@ -6,3 +6,6 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     # Boolean = true or false
     done = models.BooleanField(default=False, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
