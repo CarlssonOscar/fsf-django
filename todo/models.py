afa8e39e85f70bf64e2eee907a30c610a4e8e335
CarlssonOscar <oosccar@gmail.com>
2021-11-25 11:39:23 +0000

    Add models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    # CharField means it will have caracters or text.
+    name = models.CharField(max_length=50, null=False, blank=False)
+    # Boolean = true or false
+    done = models.BooleanField(default=False, null=False, blank=False)
