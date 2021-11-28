75fd1668495df7feec8162db3abc4e864f7c5e45
CarlssonOscar <oosccar@gmail.com>
2021-11-28 09:46:11 +0000

    a

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     path('', views.get_todo_list, name="get_todo_list"),
     path('add', views.add_item, name='add'),
     path('edit/<item_id>', views.edit_item, name='edit'),
-    path('toggle/<item_id>', views.toggle_item, name='toggle')
+    path('toggle/<item_id>', views.toggle_item, name='toggle'),
+    path('delete/<item_id>', views.delete_item, name='delete'),
 ]
 
 # This angular bracket syntax here is common in Django URLs
